5b28f2398452aeb5e85a21ed7b27c65867f4d94a
hbm <hbmin3789@naver.com>
2020-11-05 13:13:19 +0900

    [ADD] Add Key Delay Function

---
+++ SpeechRecognKeyboard/SpeechRecognKeyboard/Model/KeyPress.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognKeyboard.Model
+{
+    public class KeyPress : BindableBase
+    {
+        private ObservableCollection<KeyItem> _keyItems;
+        public ObservableCollection<KeyItem> KeyItems
+        {
+            get => _keyItems;
+            set => SetProperty(ref _keyItems, value);
+        }
+
+    }
+}
